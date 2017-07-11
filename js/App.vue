de98c9e050d535f4228b4eaabf84682793067085
Dez <elye24@gmail.com>
2017-07-11 17:13:15 +0200

    ok

--- js/App.vue
+++ js/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div id="app">
-    <div class="ui container">
-      <div class="ui segment piled center aligned">
-        <h1>Get to work now</h1>
-      </div>
+    <div id="app">
+        <div id="header">
+            <div id="header-bg"></div>
+        </div>
+        <div class="ui container">
+            <div class="ui segment piled center aligned">
+                <h1>Get to work now</h1>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
